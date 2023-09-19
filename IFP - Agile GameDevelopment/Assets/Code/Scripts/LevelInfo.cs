3fa4950b7e59ca299d429526945b8ea92a84fdc1
FionaTheMortal <66226130+FionaTheMortal@users.noreply.github.com>
2023-09-19 17:45:01 +0200

    Add code to automatically load path in level script

--- IFP - Agile GameDevelopment/Assets/Code/Scripts/LevelInfo.cs
+++ IFP - Agile GameDevelopment/Assets/Code/Scripts/LevelInfo.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class LevelInfo : MonoBehaviour
 {
-    public GameObject path;
+    [HideInInspector]
+    public Transform[] Waypoints;
 
     public void Awake()
     {
-        
+        Transform path = transform.Find("Path");
+
+        if (path != null) 
+        {
+            Waypoints = new Transform[path.childCount];
+
+            for (int i = 0; i < Waypoints.Length; i++) 
+            {
+                Waypoints[i] = path.GetChild(i);
+            }
+        }
     }
 }
