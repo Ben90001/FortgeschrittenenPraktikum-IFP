7c38fb43228fd63e8fd45619cb7751d111c2040c
FionaTheMortal <66226130+FionaTheMortal@users.noreply.github.com>
2023-09-22 14:28:35 +0200

    Add path following behavior and delete enemy when it reaches its goal

--- IFP - Agile GameDevelopment/Assets/Code/Scripts/Enemy/Enemy.cs
+++ IFP - Agile GameDevelopment/Assets/Code/Scripts/Enemy/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     [HideInInspector]
     public Transform[] Path;
 
-    public float MovementSpeed = 1.0f;
+    public float MovementSpeed = 2.0f;
 
     public float Health = 10.0f;
 
@@ -15,5 +15,59 @@ public class Enemy : MonoBehaviour
 
     public void FixedUpdate()
     {
+        bool reachedEnd = followPath();
+
+        if (reachedEnd)
+        {
+            // TODO: Handle life points etc.
+
+            Destroy(gameObject);
+        }
+    }
+
+    private bool followPath()
+    {
+        bool reachedEnd = false;
+
+        Vector3 currentPosition = transform.position;
+
+        float distanceToTravel = MovementSpeed * Time.fixedDeltaTime;
+
+        while (distanceToTravel > 0)
+        {
+            if (nextTargetIndex < Path.Length)
+            {
+                Vector3 targetPosition = Path[nextTargetIndex].position;
+
+                Vector3 delta = targetPosition - currentPosition;
+
+                float distance = delta.magnitude;
+
+                if (distance > distanceToTravel)
+                {
+                    delta *= distanceToTravel / distance;
+
+                    distance = distanceToTravel;
+                }
+                else
+                {
+                    ++nextTargetIndex;
+                }
+
+                distanceToTravel -= distance;
+
+                currentPosition += delta;
+            }
+            else
+            {
+                reachedEnd = true;
+
+                break;
+            }
+        }
+
+        transform.position = currentPosition;
+
+        return reachedEnd;
     }
 }
