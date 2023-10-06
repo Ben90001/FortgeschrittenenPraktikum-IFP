3b294495a6dff3b3453abfe9b929cc91c26bc9fa
Ben90001 <benhorn@gmx.net>
2023-10-06 09:44:11 +0200

    Created BasicTowerTests file

---
+++ IFP - Agile GameDevelopment/Assets/Tests/EditMode/BasicTower Tests.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class BasicTowerTests
+{
+    public class FixedUpdateMethodTests
+    {
+        private BasicTower BasicTower = new GameObject().AddComponent<BasicTower>();
+        
+        
+
+
+        [SetUp]
+        public void BeforeEveryTest()
+        {
+            
+
+        }
+
+        //TODO: rest of the tower
+        [Test]
+        public void ActionTimeUpperBound()
+        {
+
+        }
+    }
+}
