9eb445383c9c36303eaa510c775dd00e1df51697
Leilasaid <Leila-Said@hotmail.de>
2023-10-06 09:40:09 +0200

    checks if GetLevelKey returns correct LevelKEy for all levels (for now: level 1 and 2)

---
+++ IFP - Agile GameDevelopment/Assets/Tests/EditMode/LevelSelectionTest.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionTests
+{
+    [Test]
+    public void GetLevelKey_ReturnsCorrectLevelKeyForAllLevels()
+    {
+
+        int numberOfLevels = 2;
+
+        for (int levelID = 1; levelID <= numberOfLevels; levelID++)
+        {
+            string expectedKey = "Assets/Prefabs/Levels/Level" + levelID.ToString() + ".prefab";
+
+            string result = LevelSelection.GetLevelKey(levelID);
+
+            Assert.AreEqual(expectedKey, result);
+        }
+    }
+
+ 
+
+ 
+}
